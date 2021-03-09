cb4cd52f05650678e0a1d060fdb6ad50d595883e
Elliot Hoang <lochoang75@gmail.com>
2021-03-09 22:06:58 +0700

    Finished step 10, can run but not really know all define in Cmakelist

---
+++ Step9/MathFunctions/CMakeFiles/MathFunctions.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/elliot/learning/cmake_tutorial/Step9/MathFunctions/MathFunctions.cxx" "/home/elliot/learning/cmake_tutorial/Step9/MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EXPORTING_MYMATH"
+  "MathFunctions_EXPORTS"
+  "USE_MYMATH"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/elliot/learning/cmake_tutorial/Step9/MathFunctions/CMakeFiles/SqrtLibrary.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Step9/MathFunctions/CMakeFiles/SqrtLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../libSqrtLibrary.a"
+  "../libSqrtLibrary.pdb"
+  "CMakeFiles/SqrtLibrary.dir/mysqrt.cxx.o"
+  "Table.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SqrtLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
